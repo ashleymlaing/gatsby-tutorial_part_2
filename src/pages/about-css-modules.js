350cb97b91f0162baaf361452246e5e769654575
Ashley Marie Laing <ashleym.laing@gmail.com>
2018-01-23 14:12:17 -0500

    first commit

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./about-css-modules.module.css";
+console.log(styles);
+
+import Container from "../components/container";
+
+export default () => (
+  <Container>
+    <h1>About CSS Modules</h1>
+    <p>CSS Modules are cool</p>
+  </Container>
+);
